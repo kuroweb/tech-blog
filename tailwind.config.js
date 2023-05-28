8d64dbbc47073fd538a8fe5de3952a664b267732
kuromitsu0104 <kuromitsu0104@gmail.com>
2023-05-29 02:07:52 +0900

    style: 設定ミスを修正

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
 module.exports = {
-  // 追記
   mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './features/**/*.{js,ts,jsx,tsx}',
+  ],
+  darkMode: false,
   theme: {
     extend: {},
   },
@@ -10,4 +13,4 @@ module.exports = {
     extend: {},
   },
   plugins: [require('@tailwindcss/typography')],
-};
+}
