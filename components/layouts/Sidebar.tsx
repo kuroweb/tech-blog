2c429897f8a5d7612466ee688bb621fed7527cc7
kuromitsu0104 <kuromitsu0104@gmail.com>
2023-05-29 02:10:20 +0900

    refactor: #26 Linkの実装を修正

--- components/layouts/Sidebar.tsx
+++ components/layouts/Sidebar.tsx
@@ -84,7 +84,7 @@ const Sidebar = ({ tagList }: Props) => {
             className='py-0.5 px-2 mr-1 mb-1 text-xs text-blue-700 hover:text-white bg-transparent hover:bg-blue-500 rounded-xl border border-blue-500 hover:border-transparent'
             onClick={() => setSidebarOpen(false)}
           >
-            <Link passHref href={'/tags/' + tag.id}>
+            <Link href={'/tags/' + tag.id}>
               {tag.name}
             </Link>
           </button>
